22f988ddcd4f631dc7c764b23d8691cf9000c384
Alberto Rugnone <rugnonealberto@gmail.com>
2017-04-12 23:32:14 +0200

    first commit

---
+++ good-part/objects/objects.js
@@ -0,0 +1,13 @@
+/**
+ * Created by albertorugnone on 01/04/2017.
+ */
+var flight = {
+  airline: 'Oceanic',
+  number: '815',
+  departure: {
+    IATA: 'SYD',
+    time: ''
+  }
+}
+
+console.log(flight)
---
+++ js-patterns/global.js
@@ -0,0 +1,4 @@
+//this is an anti-pattern the global variable should be avoided
+myglobal = "hello";
+
+
